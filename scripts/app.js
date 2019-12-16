929a3cf9473dcd89354e9ee12b3d5fdceeddae70
klarv2 <propertymanagecr@gmail.com>
2019-12-16 13:05:56 -0800

    initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ dgMovieApp.config(['$qProvider', function ($qProvider) {
 
 dgMovieApp.controller('MovieController', ['$scope', '$http', function($scope, $http){
     
-    $http.get("http://www.omdbapi.com/?s=Batman&apikey=242ae804").then(function(res){
+    $http.get("https://www.omdbapi.com/?s=Batman&apikey=242ae804").then(function(res){
         $scope.movies = res.data.Search;
        // console.log($scope.movies)
     });
